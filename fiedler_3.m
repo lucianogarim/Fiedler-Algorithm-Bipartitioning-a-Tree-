4032e2789febf5cba6c4b3ad7746c9a5761a6ca2
lucianogarim <60741272+lucianogarim@users.noreply.github.com>
2020-02-14 11:54:37 -0300

    Add files via upload
    This algorithm do the same tasks than the fiedler_2. But in this version, is possible to compare all Fiedler's Cut and the Size of the Sets.
    Note that, sets of the same size do not necessarily generate the best RatioCut according to Fiedler's algorithm.

---
+++ fiedler_3.m
@@ -0,0 +1,65 @@
+Tree_Generator_main
+
+[autv,autva]=eigs(laplacian,size(laplacian,1));
+U2=autv(:,size(laplacian,1)-1);
+
+size_set=zeros(size(laplacian,1),2);
+%Fiedler's algorithm (RatioCut formulation)
+for j=1:size(laplacian,1)
+    t(j)=U2(j,1);                       %set the threshold according to the vector components
+    x = (U2 > t(j));                    %taking the elements higher than the threshold
+    x = (2 * x) - 1;                    %adjusting the vector x to values ??of 1 and -1 (indicators)
+    s=sum(x>-1);                        %defining the sizes of the sets
+    scomp=sum(x < 1);
+    size_set(j,1:2)=[s,scomp]
+    n=s+scomp;
+    for i=1:n
+        if x(i)>0
+            x(i)= sqrt((scomp)/(s*n));  %building the characteristic vectors
+        else
+            x(i)=-sqrt((s)/(scomp*n));
+        end
+    end
+    rrcut(j) = (x' * laplacian * x);    %calculating the ratiocut %%% The generated partition is in vector x
+end
+rrcut=transpose(rrcut);
+
+[M,I] = min(rrcut(rrcut>0));
+t(I)=U2(I,1);
+x = (U2 > t(I));
+x = (2 * x) - 1;
+s=sum(x>-1);
+scomp=sum(x < 1);
+n=s+scomp;
+for i=1:n
+    if x(i)>0
+        x(i)= sqrt((scomp)/(s*n));
+    else
+        x(i)=-sqrt((s)/(scomp*n));
+    end
+end
+rrcut(I) = (x' * laplacian * x);
+
+tiledlayout(2,1)
+nexttile
+G=graph(adj);
+p = plot(G);                        %Plot the result
+b=zeros(n,1);
+for i=1:n
+    if x(i)>=0
+        b(i)=i;
+    end
+end
+b = nonzeros(b');
+highlight(p,b,'NodeColor','r')
+layout(p,'force3')
+view(3)
+
+nexttile
+
+
+diff=abs(size_set(:,1)-size_set(:,2));
+var=[diff rrcut];
+
+tt=stackedplot(var);
+title('Column 1: Difference between the size of sets. Column2: Values of each Fiedler Cut')
